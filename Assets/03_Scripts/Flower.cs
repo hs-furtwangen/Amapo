2c4f4e80cdab786f91e077bbd5eb0304b859d934
Dennis Hawran <gdatmo@gmail.com>
2022-01-22 15:44:16 +0100

    Flower

---
+++ Assets/03_Scripts/Flower.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    [SerializeField] private GameObject flowerDay;
+    [SerializeField] private List<Renderer> dayObjects = new List<Renderer>();
+    [SerializeField] private GameObject flowerNight;
+    [SerializeField] private List<Renderer> nightObjects = new List<Renderer>();
+    [SerializeField] private float fadeSpeed = 0.5f;
+    [SerializeField] private float alphaThreshold = 0.1f;
+    private Daytime daytime;
+    private void Start()
+    {
+        GameManager.instance.OnDaytimeChanged += ChangeFlower;
+    }
+
+    private void Update()
+    {
+        float allAlpha = 0f;
+        foreach (Renderer dayObject in dayObjects)
+        {
+            Color color = dayObject.material.color;
+            color.a = Mathf.Clamp01(color.a + (daytime == Daytime.Day ? 1 : -1) * fadeSpeed * Time.deltaTime);
+            allAlpha += color.a;
+            dayObject.material.color = color;
+        }
+        
+        flowerDay.SetActive((allAlpha / dayObjects.Count) > alphaThreshold);
+
+        allAlpha = 0f;
+        foreach (Renderer nightObject in nightObjects)
+        {
+            Color color = nightObject.material.color;
+            color.a = Mathf.Clamp01(color.a + (daytime == Daytime.Day ? -1 : 1) * fadeSpeed * Time.deltaTime);
+            allAlpha += color.a;
+            nightObject.material.color = color;
+        }
+
+        flowerNight.SetActive((allAlpha / nightObjects.Count) > alphaThreshold);
+    }
+
+    private void ChangeFlower(Daytime daytime)
+    {
+        this.daytime = daytime;
+        // if (daytime == Daytime.Day)
+        // {
+        //     flowerDay.SetActive(true);
+        //     flowerNight.SetActive(false);
+        // }
+        // else
+        // {
+        //     flowerDay.SetActive(false);
+        //     flowerNight.SetActive(true);
+        // }
+    }
+}
