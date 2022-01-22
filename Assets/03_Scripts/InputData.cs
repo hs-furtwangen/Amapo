088af477517001f8bf791cb290c13f4abd053e80
Dennis Hawran <gdatmo@gmail.com>
2022-01-22 01:46:06 +0100

    Added Input Controller

---
+++ Assets/03_Scripts/InputData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputData
+{
+    public Vector3 Movement { get; set; }
+    public bool Jump { get; set; }
+}
